606954714f016fd634a14f11dac1f9861d99e913
samstudy <samat.study@gmail.com>
2019-03-04 22:43:39 +0600

    Added cont inf

--- fill_report.py
+++ fill_report.py
@@ -31,6 +31,30 @@ def write_infromation_into_file(my_file, information_from_json):
     work_sheet['D19'] = information_from_json['principal']['inn']
     work_sheet['D20'] = information_from_json['principal']['kpp']
     work_sheet['D21'] = information_from_json['principal']['ogrn']
+    work_sheet['F23'] = information_from_json['principal']['registration_certificate_date']
+    work_sheet['F24'] = information_from_json['principal']['registration_place']
+    work_sheet['F26'] = information_from_json['principal']['registration_place']
+    #Адрес местонахождения указанный при
+    #End
+    #Контакты Компании
+    work_sheet['F37'] = information_from_json['principal']['email']
+    work_sheet['F38'] = information_from_json['principal']['contact_phone']
+    #СВЕДЕНИЯ ОБ ОТВЕТСТВЕННЫХ ЛИЦАХ
+    work_sheet['F40'] = information_from_json['principal']['CEO']['name']
+    if information_from_json['principal']['CEO']['is_name_changed'] is False:
+        work_sheet['F41'] = 'нет'
+    else:
+        work_sheet['F41'] = 'да'
+    work_sheet['F42'] = information_from_json['principal']['CEO']['citizenship']
+    work_sheet['F43'] = information_from_json['principal']['CEO']['birth_place']
+    work_sheet['F44'] = information_from_json['principal']['CEO']['birth_date']
+    work_sheet['F45'] = information_from_json['principal']['CEO']['snils']
+    work_sheet['F46'] = information_from_json['principal']['CEO']['inn']
+    work_sheet['D48'] = information_from_json['principal']['okved_main']
+    #work_sheet['D49'] = information_from_json['principal']['okved_main']
+
+
+
     work_book.save(my_file)
 
 
